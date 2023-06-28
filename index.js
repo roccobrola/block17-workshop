f23d3781a4cf314ccadda569d6070f2080f4933a
Mark Lawrence <mark.lawrence@fullstackacademy.com>
2023-06-27 21:15:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// Before you begin coding, be sure that you:
+// a. Cleaned up the coffee_data.js file
+// b. Connect the coffee_data.js file to this index.js file.
+
+// Prompt 2:
+// Print an array of all the drinks.
+
+// Prompt 3
+// Print an array of drinks that cost 5 and under.
+
+// Prompt 4
+// Print an array of drinks that are priced at an even number.
+
+// Prompt 5
+// Print the total if you were to order one of every drink.
+
+// Prompt 6
+// Print an array with all the drinks that are seasonal.
+
+// Prompt 7
+// Print all the seasonal drinks with the words "with imported beans" after the item name.
+// For example: "affogato with imported beans"
